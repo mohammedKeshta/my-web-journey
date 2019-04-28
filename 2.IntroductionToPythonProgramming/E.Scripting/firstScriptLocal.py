68d94918164ecf06d8beba51f9ce0839c567afb4
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-04-28 16:25:41 +0200

    [F, IntroductionToPythonProgramming-Scripting] errors and excptions

---
+++ 2.IntroductionToPythonProgramming/E.Scripting/firstScriptLocal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 15:30:54 2019
+
+@author: melzanaty
+"""
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Mohammed Elzanaty
+"""
+
+
+print(snake_string * how_many_snakes)
