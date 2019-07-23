d09ee84db4b91bc9288b25383febd432222be046
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-07-23 10:37:18 +0200

    udacity-react: Reformat initial Contacts Project

--- 6.udacity-react/contacts/src/App.js
+++ 6.udacity-react/contacts/src/App.js
@@ -1,29 +1,61 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import ListContacts from './ListContacts';
+import CreateContact from './CreateContact';
+import * as ContactsAPI from './utils/ContactsAPI';
 
+class App extends Component {
+  state = {
+    contacts: []
+  };
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState({ contacts });
+    });
+  }
+  removeContact = contact => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }));
 
-const ContactList = ({contacts}) => {
-    return (<ol>
-        { contacts.map((person, index) => <li key={index}> { person.name }</li>) }
-    </ol>);
-};
+    ContactsAPI.remove(contact);
+  };
 
-function App() {
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }));
+    });
+  }
+
+  render() {
     return (
-        <div className="App">
-            <ContactList contacts={[
-                {name: 'Mohammed'},
-                {name: 'Yasmeen'},
-                {name: 'Hamsa'},
-            ]}/>
-
-            <ContactList contacts={[
-                {name: 'Elzanaty'},
-                {name: 'Hamsa'},
-                {name: 'Hamza'},
-            ]}/>
-        </div>
+      <div>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListContacts
+              onDeleteContact={this.removeContact}
+              contacts={this.state.contacts}
+            />
+          )}
+        />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push('/');
+              }}
+            />
+          )}
+        />
+      </div>
     );
+  }
 }
 
 export default App;
