d70b8c25df861fa081e7894981bb4bbccd8e5781
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-07-28 21:25:58 +0200

    linkedin-mean-stack-js-dev: building website using node/express -> Adding Routing with logic with a pure Node.js

--- 7.linkedin-mean-stack-js-dev/3.building-a-website-with-node-js-and-express-js-2/server.js
+++ 7.linkedin-mean-stack-js-dev/3.building-a-website-with-node-js-and-express-js-2/server.js
@@ -2,10 +2,29 @@ const http = require("http");
 const url = require("url");
 
 const handler = (req, res) => {
-  const parsedURL = url.parse(req.url, true);
-  if (parsedURL.pathname === "/") {
+  const { pathname, query } = url.parse(req.url, true);
+  if (pathname === "/") {
     res.writeHead(200, { "Content-type": "text/plain" });
     res.write("Hello, from web-server \n");
+  } else if (pathname === "/time") {
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.write(`Time Now is  ${new Date().toString()} \n`);
+  } else if (pathname === "/hello") {
+    if (!query.name) {
+      res.writeHead(400, { "Content-type": "text/plain" });
+      return res.end();
+    }
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.write(`Hello, ${query.name}`);
+  } else if (pathname.startsWith("/user/")) {
+    const regex = new RegExp("/user/(.+)");
+    const matches = regex.exec(pathname);
+    if (!matches && !matches[1]) {
+      res.writeHead(400, { "Content-type": "text/plain" });
+      return res.end();
+    }
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.write(`Hello,  this is page profile of ${matches[1]}`);
   } else {
     res.writeHead(404);
   }
