5f2888b683b7ceb273b3b8eb83eb40d5c4760a8b
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-08-01 16:30:49 +0200

    linkedin-mean-stack-js-dev: building website using node/express -> adding engine template to conference project

--- 7.linkedin-mean-stack-js-dev/3.building-a-website-with-node-js-and-express-js-2/conference/index.js
+++ 7.linkedin-mean-stack-js-dev/3.building-a-website-with-node-js-and-express-js-2/conference/index.js
@@ -3,10 +3,17 @@ const path = require("path");
 const app = express();
 const routes = require("./routes");
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/", routes);
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "./views"));
 
+if (app.get("env") === "development") app.locals.pretty = true;
+
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
+app.get("/", (req, res) => {
+  res.render("index", { title: "Conference" });
+});
+app.use("/", routes);
 app.listen(3000);
 module.exports = app;
