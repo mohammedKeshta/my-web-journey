fd28db87177eb97be6fb5abfba118ed5d4c373ab
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-08-01 16:49:02 +0200

    linkedin-mean-stack-js-dev: building website using node/express -> conference project - Creating Custom Error page

--- 7.linkedin-mean-stack-js-dev/3.building-a-website-with-node-js-and-express-js-2/conference/index.js
+++ 7.linkedin-mean-stack-js-dev/3.building-a-website-with-node-js-and-express-js-2/conference/index.js
@@ -1,3 +1,4 @@
+const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -15,5 +16,17 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Conference" });
 });
 app.use("/", routes);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => next(createError(404)));
+// Error Handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 app.listen(3000);
 module.exports = app;
