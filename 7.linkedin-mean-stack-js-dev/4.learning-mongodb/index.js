01b418c14a2f17547662d4ccc8133a72d1df2fa3
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-08-09 17:18:49 +0200

    linkedin-become-a-mern-stack-javascript-developer: learning-mongodb -> make 2 API with get request

--- 7.linkedin-mean-stack-js-dev/4.learning-mongodb/index.js
+++ 7.linkedin-mean-stack-js-dev/4.learning-mongodb/index.js
@@ -1,19 +1,93 @@
+"use strict";
+
 const MongoClient = require("mongodb").MongoClient;
+const Hapi = require("@hapi/hapi");
 
 const url = "mongodb://localhost:27017/learning_mongo";
 
-const findDocuments = (db, callback) => {
-  const tours = db.collection("tours");
-  tours
-    .find({ tourName: "Monterey to Santa Barbara Tour" })
-    .toArray((err, docs) => {
-      console.log(docs);
-      callback;
-    });
-};
-MongoClient.connect(url, (err, db) => {
-  console.log("Server Connected Successfully");
-  findDocuments(db, () => {
-    db.close();
+const init = async tours => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost"
   });
+
+  server.route([
+    // Get tour list
+    {
+      method: "GET",
+      path: "/api/tours",
+      config: { json: { space: 2 } },
+      handler: async (request, reply) => {
+        return new Promise(resolve => {
+          const findObject = {};
+          for (let key in request.query) {
+            findObject[key] = request.query[key];
+          }
+          tours.find(findObject).toArray((error, tours) => {
+            resolve(tours);
+          });
+        });
+      }
+    },
+    // Add new tour
+    {
+      method: "POST",
+      path: "/api/tours",
+      handler: (request, reply) => {}
+    },
+    // Get a single tour
+    {
+      method: "GET",
+      path: "/api/tours/{name}",
+      handler: (request, reply) => {
+        return new Promise(resolve => {
+          tours.findOne(
+            { tourName: request.params.name },
+            (mongoError, tours) => {
+              resolve(tours);
+            }
+          );
+        });
+      }
+    },
+    // Update a single tour
+    {
+      method: "PUT",
+      path: "/api/tours/{name}",
+      handler: (request, reply) => {}
+    },
+    // Delete a single tour
+    {
+      method: "DELETE",
+      path: "/api/tours/{name}",
+      handler: (request, reply) => {}
+    },
+    // Home page
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, reply) => {
+        return "Hello world from Hapi/Mongo example.";
+      }
+    }
+  ]);
+
+  await server.start(err => {
+    if (err) {
+      throw err;
+    }
+    console.log("Server running on %s", server.info.uri);
+    console.log(`Server Running at PORT ${server.info.port}`);
+  });
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+MongoClient.connect(url, (err, db) => {
+  console.log("connected correctly to server");
+  let collection = db.collection("tours");
+  init(collection);
 });
