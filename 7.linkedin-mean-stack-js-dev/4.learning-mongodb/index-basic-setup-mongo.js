0ad397d5c3a1ee907ca6a6184b9ddaa6bd9e34dc
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-08-13 15:59:22 +0200

    udacity-full-stack-developer: intro-to-html -> init milestone

--- 7.linkedin-mean-stack-js-dev/4.learning-mongodb/index-basic-setup-mongo.js
+++ 7.linkedin-mean-stack-js-dev/4.learning-mongodb/index-basic-setup-mongo.js
@@ -4,12 +4,10 @@ const url = "mongodb://localhost:27017/learning_mongo";
 
 const findDocuments = (db, callback) => {
   const tours = db.collection("tours");
-  tours
-    .find({ tourName: "Monterey to Santa Barbara Tour" })
-    .toArray((err, docs) => {
-      console.log(docs);
-      callback;
-    });
+  tours.find({ tourName: "Monterey to Santa Barbara Tour" }).toArray((err, docs) => {
+    console.log(docs);
+    callback;
+  });
 };
 MongoClient.connect(url, (err, db) => {
   console.log("Server Connected Successfully");
--- 7.linkedin-mean-stack-js-dev/4.learning-mongodb/learning_mongo-ex-files/Chapter3/03_01/Finished/index.js
+++ 7.linkedin-mean-stack-js-dev/4.learning-mongodb/learning_mongo-ex-files/Chapter3/03_01/Finished/index.js
@@ -5,12 +5,10 @@ var url = "mongodb://localhost:27017/learning_mongo";
 var findDocuments = function(db, callback) {
   var collection = db.collection("tours");
 
-  collection
-    .find({ tourPackage: "Snowboard Cali" })
-    .toArray(function(err, docs) {
-      console.log(docs);
-      callback;
-    });
+  collection.find({ tourPackage: "Snowboard Cali" }).toArray(function(err, docs) {
+    console.log(docs);
+    callback;
+  });
 };
 
 MongoClient.connect(url, function(err, db) {
