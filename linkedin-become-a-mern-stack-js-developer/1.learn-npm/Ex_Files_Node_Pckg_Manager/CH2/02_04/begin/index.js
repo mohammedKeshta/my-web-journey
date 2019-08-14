6465afbf3a67d8653726f2b84990185b8fca06f3
MohammedElzanaty <mohammedelzanaty129@gmail.com>
2019-08-14 12:36:36 +0200

    garbage files

--- linkedin-become-a-mern-stack-js-developer/1.learn-npm/Ex_Files_Node_Pckg_Manager/CH2/02_04/begin/index.js
+++ linkedin-become-a-mern-stack-js-developer/1.learn-npm/Ex_Files_Node_Pckg_Manager/CH2/02_04/begin/index.js
@@ -1,12 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
