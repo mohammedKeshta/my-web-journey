ac38b48c9fcb8960c94459ba250d51313e1d26c0
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-08-16 18:08:56 +0200

    linkedin-become-react-developer: learning-redux -> Create containers for form components

--- linkedin-become-react-developer/3.learning-redux/Ski/src/components/containers/AddDayForm.js
+++ linkedin-become-react-developer/3.learning-redux/Ski/src/components/containers/AddDayForm.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import { connect } from 'react-redux';
 import AddDayForm from '../ui/AddDayForm';
+import { addDay, suggestResortNames, clearSuggestions } from '../../redux/actions';
 import { withRouter } from 'react-router';
 
-export default withRouter(props => (
-  <AddDayForm
-    suggestions={[]}
-    fetching={false}
-    router={props.router}
-    onNewDay={day => console.log('todo: add day', day)}
-    onChange={value => console.log('todo: suggest', value)}
-    onClear={() => console.log('todo: clear suggestions')}
-  />
-));
+const mapStateToProps = (status, props) => ({
+  suggestions: status.resortNames.suggestions,
+  fetching: status.resortNames.fetching,
+  router: props.match.router
+});
+
+const mapDispatchToProps = dispatch => ({
+  onNewDay({ resort, date, powder, backcountry }) {
+    dispatch(addDay(resort, date, powder, backcountry));
+  },
+  onChange(value) {
+    if (value) {
+      dispatch(suggestResortNames(value));
+    } else {
+      dispatch(clearSuggestions());
+    }
+  },
+  onClear() {
+    dispatch(clearSuggestions());
+  }
+});
+
+const Container = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddDayForm);
+export default withRouter(Container);
