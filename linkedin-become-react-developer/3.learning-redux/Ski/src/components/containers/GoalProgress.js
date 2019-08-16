ad1fe60fb2ee60c0ee87449f553acfecbca0ccb1
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-08-16 17:51:59 +0200

    linkedin-become-react-developer: learning-redux -> Connecting The Goal Component

--- linkedin-become-react-developer/3.learning-redux/Ski/src/components/containers/GoalProgress.js
+++ linkedin-become-react-developer/3.learning-redux/Ski/src/components/containers/GoalProgress.js
@@ -1,4 +1,15 @@
-import React from 'react';
 import GoalProgress from '../ui/GoalProgress';
+import { connect } from 'react-redux';
+import { setGoal } from '../../redux/actions';
 
-export default () => <GoalProgress current={10} goal={20} onNewGoal={goal => console.log('todo: change goal', goal)} />;
+const mapStateToProps = state => ({ goal: state.allSkiDays.length, current: state.goal });
+const mapDispatchToProps = dispatch => ({
+  onNewGoal(goal) {
+    dispatch(setGoal(goal));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GoalProgress);
