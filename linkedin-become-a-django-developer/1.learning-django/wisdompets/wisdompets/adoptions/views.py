2a1f77edb007ffde2a8da7c6c43245db8b38424d
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-09-10 21:54:24 +0200

    linkedin-become-a-django-developer: learning-django -> 3. Building URL Handlers and Views

--- linkedin-become-a-django-developer/1.learning-django/wisdompets/wisdompets/adoptions/views.py
+++ linkedin-become-a-django-developer/1.learning-django/wisdompets/wisdompets/adoptions/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+from .models import Pet
+
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {"pets": pets})
+
+
+def pet_detail(request, id):
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet Not Found')
+    return render(request, 'pet_details.html', {"pet": pet})
