69a80fce7a06f89c8b9b96770cf4ab40b7cfab47
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2019-09-18 22:24:19 +0200

    linkedin-become-a-programmer-foundations: Programming Foundations: Fundamentals -> 2. Data Types

---
+++ linkedin-become-a-programmer-foundations/1.programming-foundations-fundamentals-3/data_types.py
@@ -0,0 +1,19 @@
+email_address = "mohammedelzanaty129@gmail.com"
+age = 26
+gender = 'M'
+status = None
+isMarried = False
+
+print(f"{email_address} type of {type(email_address)}")
+print(f"{age} type of {type(age)}")
+print(f"{gender} type of {type(gender)}")
+print(f"{status} type of {type(status)}")
+print(f"{isMarried} type of {type(isMarried)}")
+
+print(2 + 3)  # 5
+print(2 * 3)  # 6
+print(2 ** 3)  # 8
+print(3 / 2)  # 1.5
+print(2 // 2)  # 1
+print(5 % 3)  # 2
+print(5 - 3)  # 2
