2c9f8b81a4a39c22715a1332ccf8796dff1970b3
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-11-09 22:41:58 +0200

    Frontend-Masters: Complete Intro to React, v5 ->  Pure React

--- FrontendMasters/react/complete-react-v5/README.md
+++ FrontendMasters/react/complete-react-v5/README.md
@@ -1,1 +1,5 @@
 # [_**Complete Intro to React, v5**_](https://frontendmasters.com/courses/complete-react-v5/)
+
+# Links 
+   - [Code Repository](https://github.com/btholt/complete-intro-to-react-v5)
+   - [Course Website](https://btholt.github.io/complete-intro-to-react-v5/) 
---
+++ FrontendMasters/react/complete-react-v5/adopt-me/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.scss';
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', { className: 'pet' }, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed)
+  ]);
+};
+const App = () => {
+  return React.createElement(
+    'div',
+    { className: 'App section' },
+    React.createElement('h1', null, 'Adopt Me'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese'
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel'
+    }),
+    React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mixed' })
+  );
+};
+
+export default App;
