77bc1c9edb24276afac25deee3d5cfdb7e8fe24b
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-11-09 23:44:53 +0200

    Frontend-Masters: Complete Intro to React, v5 ->   Convert project to JSX, add React-specific ESLint config

--- FrontendMasters/react/complete-react-v5/adopt-me/src/App.js
+++ FrontendMasters/react/complete-react-v5/adopt-me/src/App.js
@@ -3,21 +3,13 @@ import './App.scss';
 import Pet from './components/Pet';
 
 const App = () => {
-  return React.createElement(
-    'div',
-    { className: 'App section' },
-    React.createElement('h1', null, 'Adopt Me'),
-    React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese'
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cockatiel'
-    }),
-    React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mixed' })
+  return (
+    <div className="App section">
+      <h1 className="text-center">Adopt Me</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
   );
 };
 
--- FrontendMasters/react/complete-react-v5/adopt-me/src/components/Pet.js
+++ FrontendMasters/react/complete-react-v5/adopt-me/src/components/Pet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', { className: 'pet' }, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ]);
+  return (
+    <div className="pet">
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 };
 
 export default Pet;
