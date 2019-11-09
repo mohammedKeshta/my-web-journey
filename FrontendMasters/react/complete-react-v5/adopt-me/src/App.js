5367f93f812f6d391ff24080f5e51c181119ce5b
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-11-09 23:27:24 +0200

    Frontend-Masters: Complete Intro to React, v5 ->   Add npm scripts eslint peitter and gitignore

--- FrontendMasters/react/complete-react-v5/adopt-me/src/App.js
+++ FrontendMasters/react/complete-react-v5/adopt-me/src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './App.scss';
+import Pet from './components/Pet';
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', { className: 'pet' }, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ]);
-};
 const App = () => {
   return React.createElement(
     'div',
