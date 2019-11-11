0bf8caa724d31dd908cbb92b6422d362694aa177
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-11-11 10:45:53 +0200

    Frontend-Masters: Complete Intro to React, v5  -> add @reach/router and details component

--- FrontendMasters/react/complete-react-v5/adopt-me/src/App.js
+++ FrontendMasters/react/complete-react-v5/adopt-me/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.scss';
+import { Router, Link } from '@reach/router';
 import SearchParams from './components/SearchParams';
+import Details from './components/Details';
 
 const App = () => {
   return (
     <React.StrictMode>
       <div className="App section">
-        <h1 className="text-center">Adopt Me</h1>
-        <SearchParams />
+        <Link to="/">
+          <h1 className="logo">Adopt Me</h1>
+        </Link>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
