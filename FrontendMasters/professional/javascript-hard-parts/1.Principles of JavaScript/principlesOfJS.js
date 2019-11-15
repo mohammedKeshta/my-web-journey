226f2b88ac4f0a9286048148b00b287d027ee7f1
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-11-15 14:23:02 +0200

    Frontend-Masters: JavaScript: The Hard Parts  -> Principles of JavaScript

---
+++ FrontendMasters/professional/javascript-hard-parts/1.Principles of JavaScript/principlesOfJS.js
@@ -0,0 +1,16 @@
+const num = 2;
+
+function multiplyBy2(inputNumber) {
+  const result = inputNumber * 2;
+  return result;
+}
+
+const name = "Will";
+const output = multiplyBy2(4);
+const newOutput = multiplyBy2(10);
+
+console.log("====================================");
+console.log("num is ", num);
+console.log("output is ", output);
+console.log("newOutput is ", newOutput);
+console.log("multiplyBy2 is ", multiplyBy2);
