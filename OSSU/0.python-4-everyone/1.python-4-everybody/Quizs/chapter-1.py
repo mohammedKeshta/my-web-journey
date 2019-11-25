3b9e1bddb69399ff38e8e4546835a5f4a6e4932d
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-11-25 19:22:34 +0200

    Python for EveryOne: Exploring Data in Python 3 -> Python for Everybody : Week Three

---
+++ OSSU/0.python-4-everyone/1.python-4-everybody/Quizs/chapter-1.py
@@ -0,0 +1,47 @@
+"""
+http://bit.ly/2Olf7EF
+
+# 1
+Q. When Python is running in the interactive mode and displaying the chevron prompt (>>>) - what question is Python asking you?
+A. What Python script would you like me to run?
+
+# 2
+Q. What will the following program print out:
+ x = 15
+ x = x + 5
+ print(x)
+
+A. 20
+
+# 3
+Q. Python scripts (files) have names that end with:
+A. .py
+
+# 4
+Q. Which of these words is a reserved word in Python ?
+A. While
+
+# 5
+Q. What is the proper way to say “good-bye” to Python?
+A. quite()
+
+# 6
+Q. Which of the parts of a computer actually executes the program instructions?
+A. Central Processing Unit (CPU)
+
+# 7
+Q. What is "code" in the context of this course?
+A. A sequence of instructions in a programming language
+
+# 8
+Q. A USB memory stick is an example of which of the following components of computer architecture?
+A. Secondary Memory
+
+# 9
+Q. What is the best way to think about a "Syntax Error" while programming?
+A. The computer did not understand the statement that you entered
+
+# 10
+Q. Which of the following is not one of the programming
+A. Random steps
+"""
