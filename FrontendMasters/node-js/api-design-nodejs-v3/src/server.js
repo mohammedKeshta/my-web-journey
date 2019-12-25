244d75fb98f77fe4209efaa3e9a5a0f9eb49b5f8
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-12-25 10:39:25 +0200

    Frontend-Masters:  API Design in Node.js, v3  -> Lesson One

--- FrontendMasters/node-js/api-design-nodejs-v3/src/server.js
+++ FrontendMasters/node-js/api-design-nodejs-v3/src/server.js
@@ -12,4 +12,39 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.use('/api/item', router)
+const route = express.Router()
+
+// Logging Middleware
+const log = (req, res, next) => {
+  console.log(`Logging Middleware`)
+  next()
+}
+
+route.get('/me', (req, res) => {
+  res.json({ data: 'me' })
+})
+
+app.use('/api', route)
+
+// GET
+app.get('/', log, (req, res) => {
+  res.json({ message: 'All Messages' })
+})
+// PUT
+app.put('/', (req, res) => {
+  res.json({ message: 'Message Updated' })
+})
+// POST
+app.post('/', (req, res) => {
+  const MESSAGE = req.body.message || 'Message Created'
+  res.json({ message: MESSAGE })
+})
+// DELETE
+app.delete('/', (req, res) => {
+  res.json({ message: 'Message Deleted' })
+})
+
+export const start = () => {
+  app.listen(3000, () => console.log(`server is running at port 3000`))
+}
