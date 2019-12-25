82916cc080eabbf709d430e88edebe7caa3e1bf2
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-12-25 10:48:45 +0200

    Frontend-Masters:  API Design in Node.js, v3  -> Lesson Two

--- FrontendMasters/node-js/api-design-nodejs-v3/src/resources/item/item.router.js
+++ FrontendMasters/node-js/api-design-nodejs-v3/src/resources/item/item.router.js
@@ -2,4 +2,27 @@ import { Router } from 'express'
 
 const router = Router()
 
+// api/item
+router
+  .route('/')
+  .get((req, res) => {
+    res.json({ status: 200, data: 'All Items' })
+  })
+  .post((req, res) => {
+    res.json({ status: 200, data: 'Item Created' })
+  })
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.json({ status: 200, data: `Items ${req.params.id}` })
+  })
+  .put((req, res) => {
+    res.json({ status: 200, data: `item ${req.params.id} updated` })
+  })
+  .delete((req, res) => {
+    res.json({ status: 200, data: `item ${req.params.id} deleted` })
+  })
+
 export default router
--- FrontendMasters/node-js/api-design-nodejs-v3/src/server.js
+++ FrontendMasters/node-js/api-design-nodejs-v3/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
+import config from './config'
 import cors from 'cors'
+import { connect } from './utils/db'
+import itemRouter from './resources/item/item.router'
 
 export const app = express()
 
@@ -12,39 +15,15 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.use('/api/item', router)
-const route = express.Router()
+app.use('/api/item', itemRouter)
 
-// Logging Middleware
-const log = (req, res, next) => {
-  console.log(`Logging Middleware`)
-  next()
-}
-
-route.get('/me', (req, res) => {
-  res.json({ data: 'me' })
-})
-
-app.use('/api', route)
-
-// GET
-app.get('/', log, (req, res) => {
-  res.json({ message: 'All Messages' })
-})
-// PUT
-app.put('/', (req, res) => {
-  res.json({ message: 'Message Updated' })
-})
-// POST
-app.post('/', (req, res) => {
-  const MESSAGE = req.body.message || 'Message Created'
-  res.json({ message: MESSAGE })
-})
-// DELETE
-app.delete('/', (req, res) => {
-  res.json({ message: 'Message Deleted' })
-})
-
-export const start = () => {
-  app.listen(3000, () => console.log(`server is running at port 3000`))
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
