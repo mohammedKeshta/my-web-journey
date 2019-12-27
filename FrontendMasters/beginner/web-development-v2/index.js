a67c49d8053292ed174a404065e2bf7dd4548255
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-12-27 13:52:58 +0200

    Frontend-Masters:  Complete Intro to Web Development, v2  -> Objects and Arrays

--- FrontendMasters/beginner/web-development-v2/index.js
+++ FrontendMasters/beginner/web-development-v2/index.js
@@ -1,3 +1,7 @@
 const scope = require('./javascript/scope');
+const builtin = require('./javascript/builtin');
+const objectAndArrays = require('./javascript/objects_array');
 
 // scope.run();
+// builtin.run();
+objectAndArrays.run();
