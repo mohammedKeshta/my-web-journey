2be1746808b73a4c5e93be3d3db1daa685de7611
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2019-12-27 19:03:31 +0200

    Frontend-Masters:  Complete Intro to Web Development, v2  -> AJAX and parcel

--- FrontendMasters/beginner/web-development-v2/index.js
+++ FrontendMasters/beginner/web-development-v2/index.js
@@ -2,6 +2,27 @@ const scope = require('./javascript/scope');
 const builtin = require('./javascript/builtin');
 const objectAndArrays = require('./javascript/objects_array');
 
-// scope.run();
-// builtin.run();
+scope.run();
+builtin.run();
 objectAndArrays.run();
+
+const DOG_URL = 'https://dog.ceo/api/breeds/image/random';
+const dogContainer = document.querySelector('.dog-container');
+
+const createImage = url => {
+  const img = document.createElement('img');
+  img.src = url;
+  img.alt = 'Random Dog';
+  dogContainer.appendChild(img);
+};
+
+const addNewDoggo = () => {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(response => response.json())
+    .then(response => {
+      createImage(response.message);
+    });
+};
+
+document.querySelector('.add-dog').addEventListener('click', () => addNewDoggo());
