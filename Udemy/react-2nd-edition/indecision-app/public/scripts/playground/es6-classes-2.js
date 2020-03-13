64ac1519008bc5de5637892ab2e719b51b03b50d
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-03-13 14:20:04 +0200

    UDEMY: react-2nd-edition => Section React Components - ES6 classes 2

--- Udemy/react-2nd-edition/indecision-app/public/scripts/playground/es6-classes-2.js
+++ Udemy/react-2nd-edition/indecision-app/public/scripts/playground/es6-classes-2.js
@@ -6,6 +6,10 @@ function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) ===
 
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
@@ -50,22 +54,78 @@ var Person = /*#__PURE__*/function () {
 var Student = /*#__PURE__*/function (_Person) {
   _inherits(Student, _Person);
 
-  function Student() {
+  function Student(name, age, major) {
+    var _this;
+
     _classCallCheck(this, Student);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(Student).call(this));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Student).call(this, name, age));
+    _this.major = major;
+    return _this;
   }
+
+  _createClass(Student, [{
+    key: "hasMajor",
+    value: function hasMajor() {
+      return !!this.major;
+    }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      var description = _get(_getPrototypeOf(Student.prototype), "getDescription", this).call(this);
+
+      description += this.hasMajor() ? " Their major is ".concat(this.major) : '';
+      return description;
+    }
+  }]);
 
   return Student;
 }(Person);
 
-var me = new Student('Mohammed Elzanaty', 26);
+var Traveler = /*#__PURE__*/function (_Person2) {
+  _inherits(Traveler, _Person2);
+
+  function Traveler(name, age, homeLocation) {
+    var _this2;
+
+    _classCallCheck(this, Traveler);
+
+    _this2 = _possibleConstructorReturn(this, _getPrototypeOf(Traveler).call(this, name, age));
+    _this2.homeLocation = homeLocation;
+    return _this2;
+  }
+
+  _createClass(Traveler, [{
+    key: "hasHomeLocation",
+    value: function hasHomeLocation() {
+      return !!this.homeLocation;
+    }
+  }, {
+    key: "getGreeting",
+    value: function getGreeting() {
+      var description = _get(_getPrototypeOf(Traveler.prototype), "getGreeting", this).call(this);
+
+      description += this.hasHomeLocation() ? " I'm visiting from ".concat(this.homeLocation) : '';
+      return description;
+    }
+  }]);
+
+  return Traveler;
+}(Person);
+
+var me = new Student('Mohammed Elzanaty', 26, 'Computer Science');
+var meTraveler = new Traveler('Mohammed Elzanaty', 26, 'Egypt');
 console.log(me);
 console.log(me.getGreeting());
 console.log(me.getDescription());
+console.log(me.hasMajor());
+console.log(meTraveler.getGreeting());
 console.log('--------------------');
 var other = new Student();
+var otherTraveler = new Traveler();
 console.log(other);
 console.log(other.getGreeting());
 console.log(other.getDescription());
+console.log(other.hasMajor());
+console.log(otherTraveler.getGreeting());
 console.log('-------END----------');
--- Udemy/react-2nd-edition/indecision-app/src/playground/es6-classes-2.js
+++ Udemy/react-2nd-edition/indecision-app/src/playground/es6-classes-2.js
@@ -17,23 +17,59 @@ class Person {
 }
 
 class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
 
-  constructor() {
-    super();
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+    description += this.hasMajor() ? ` Their major is ${this.major}` : '';
+    return description;
   }
 }
 
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
 
-const me = new Student('Mohammed Elzanaty', 26);
+  hasHomeLocation() {
+    return !!this.homeLocation;
+  }
+
+  getGreeting() {
+    let description = super.getGreeting();
+    description += this.hasHomeLocation() ? ` I'm visiting from ${this.homeLocation}` : '';
+    return description;
+  }
+}
+
+const me = new Student('Mohammed Elzanaty', 26, 'Computer Science');
+const meTraveler = new Traveler('Mohammed Elzanaty', 26, 'Egypt');
+
 console.log(me);
 console.log(me.getGreeting());
 console.log(me.getDescription());
+console.log(me.hasMajor());
+
+console.log(meTraveler.getGreeting());
 
 console.log('--------------------');
 
 const other = new Student();
+const otherTraveler = new Traveler();
+
 console.log(other);
 console.log(other.getGreeting());
 console.log(other.getDescription());
+console.log(other.hasMajor());
+
+console.log(otherTraveler.getGreeting());
 
 console.log('-------END----------');
