608e1f530969fe34f048a08dc6e5383ce14da73d
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-03-17 23:25:16 +0200

    UDEMY: The Complete Node.js Developer Course -> Section 3: Node.js Module -> importing npm modules

--- Udemy/the-complete-nodejs-developer-course-2/notes-app/app.js
+++ Udemy/the-complete-nodejs-developer-course-2/notes-app/app.js
@@ -1,9 +1,17 @@
 const utils = require('./utilities')
 const getNotes = require('./notes')
+const validator = require('validator')
 
+/* use another module */
 console.log(utils.name) // Mohammed
 console.log(utils.add(1, 2)) // 3
 console.log(utils.add(1, 2, 3, 4, 5)) // 15
 
+/* use our own files */
 const msg = getNotes()
 console.log(msg)
+
+/*use node modules*/
+console.log(validator.isEmail('mo@mo.com')) // true
+console.log(validator.isURL('https://google.com')) // true
+console.log(validator.isURL('google')) // false
