01be3858f54ca55a56119ff168b3cc17651358fa
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-03-21 14:36:21 +0200

    UDEMY: The Complete Node.js Developer Course -> Section 6: Asynchronous Node.js -> Handling Errors

--- Udemy/the-complete-nodejs-developer-course-2/weather-app/app.js
+++ Udemy/the-complete-nodejs-developer-course-2/weather-app/app.js
@@ -1,14 +1,51 @@
-const { endpoint, apiKey } = require('./config')
+const { MAP_BOX_API_URL, MAP_BOX_ACCESS_TOKEN, DARK_SKY_API_URL, DARK_SKY_SECRET_KEY } = require('./config')
 const axios = require('axios')
 
-axios
-    .get(`${endpoint}/forecast/${apiKey}/28.2567928,33.6287564`)
-    .then(function(response) {
-        // handle success
-        const { temperature, precipProbability } = response.data.currently
-        console.log(`it's currently ${temperature} degree out. There is a ${precipProbability} chance to rain.`)
-    })
-    .catch(function(error) {
+async function getWeather(lattude, lngtude) {
+    try {
+        const response = await axios.get(`${DARK_SKY_API_URL}/forecast/${DARK_SKY_SECRET_KEY}/${lattude}, ${lngtude}`)
+        const { currently, daily } = response.data
+        console.log(
+            `${daily.summary}, it's currently ${currently.temperature} degree out. There is a ${currently.precipProbability} chance to rain.`
+        )
+    } catch (error) {
         // handle error and show message
-        console.log(`error: ${error.message}`)
-    })
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            const { data, status } = error.response
+            console.log(`Server reply with error: ${data.error} with status: ${status}`)
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log(`unable to connect to weather services, error: ${error.message}`)
+        }
+    }
+}
+
+async function getGeoCodeLocation(query) {
+    try {
+        const response = await axios.get(`${MAP_BOX_API_URL}${query}.json?limit=1&access_token=${MAP_BOX_ACCESS_TOKEN}`)
+        const { features } = response.data
+        if (features && features.length !== 0) {
+            const LATITUDE = features[0].center[0]
+            const LONGITUDE = features[0].center[1]
+            console.log(`LATITUDE:${LATITUDE} and LONGITUDE:${LONGITUDE} for ${query}`)
+            await getWeather(LATITUDE, LONGITUDE)
+        } else {
+            console.log('try another longitude,latitude')
+        }
+    } catch (error) {
+        // handle error and show message
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            const { data, status } = error.response
+            console.log(`Server reply with error: ${data.error} with status: ${status}`)
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log(`unable to connect to map-box services, error: ${error.message}`)
+        }
+    }
+}
+
+getGeoCodeLocation('Cairo, Egpyt')
--- Udemy/the-complete-nodejs-developer-course-2/weather-app/config.js
+++ Udemy/the-complete-nodejs-developer-course-2/weather-app/config.js
@@ -2,8 +2,9 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-
 module.exports = {
-    endpoint: process.env.API_URL,
-    apiKey: process.env.SECRET_KEY,
+    DARK_SKY_API_URL: process.env.DARK_SKY_API_URL,
+    DARK_SKY_SECRET_KEY: process.env.DARK_SKY_SECRET_KEY,
+    MAP_BOX_API_URL: process.env.MAP_BOX_API_URL,
+    MAP_BOX_ACCESS_TOKEN: process.env.MAP_BOX_ACCESS_TOKEN,
 }
