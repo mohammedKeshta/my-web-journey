9127a18e07644bc420ce8ec2751e9b8e1527907e
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-03-21 11:36:53 +0200

    UDEMY: The Complete Node.js Developer Course -> Section 6: Asynchronous Node.js -> Asynchronous Basics

---
+++ Udemy/the-complete-nodejs-developer-course-2/weather-app/app.js
@@ -0,0 +1,13 @@
+const LOG = console.log
+
+LOG('Weather App Starting...')
+
+setTimeout(() => {
+    LOG('2 Second Timer')
+}, 2000)
+
+setTimeout(() => {
+  LOG('0 Second Timer')
+}, 0)
+
+LOG('Weather App Stop')
