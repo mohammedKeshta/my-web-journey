827e9203709b88719aff4f782e110dc154404f20
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-03-28 11:41:00 +0200

    UDEMY: The Complete Node.js Developer Course -> Section 7: Intro: Web Servers -> Serving up Static Assets

--- Udemy/the-complete-nodejs-developer-course-2/web-server/src/app.js
+++ Udemy/the-complete-nodejs-developer-course-2/web-server/src/app.js
@@ -1,6 +1,10 @@
+const path = require('path')
 const express = require('express')
 const { PORT } = require('../config/config')
 const app = express()
+
+const PUBLIC_PATH = path.join(__dirname, '../public')
+app.use(express.static(PUBLIC_PATH))
 
 app.get('', (req, res) => {
     res.send('<h1>Hello Express</h1>')
@@ -8,17 +12,9 @@ app.get('', (req, res) => {
 
 app.get('/weather', (req, res) => {
     res.json({
-        name : 'Mohammed',
-        age: 26
+        name: 'Mohammed',
+        age: 26,
     })
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help Page')
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>')
 })
 
 app.listen(3000, () => {
