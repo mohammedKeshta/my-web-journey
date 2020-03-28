7284f4e5600334aa4b85226df8e046b47bd7051d
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-03-28 14:24:14 +0200

    UDEMY: The Complete Node.js Developer Course -> Section 7: Intro: Web Servers -> Advanced Templating and Styling

--- Udemy/the-complete-nodejs-developer-course-2/web-server/src/app.js
+++ Udemy/the-complete-nodejs-developer-course-2/web-server/src/app.js
@@ -1,38 +1,60 @@
-const path = require('path')
-const express = require('express')
+const path = require('path'),
+    express = require('express'),
+    //Loads the handlebars module
+    hbs = require('express-handlebars')
 const { PORT } = require('../config/config')
+
+// Initialize Express app
 const app = express()
-
-const PUBLIC_PATH = path.join(__dirname, '../public')
-
+//Sets our app to use the handlebars engine
 app.set('view engine', 'hbs')
-app.use(express.static(PUBLIC_PATH))
+//Sets handlebars configurations
+app.engine(
+    'hbs',
+    hbs({
+        extname: 'hbs',
+        layoutsDir: path.join(__dirname, '../views/layouts'),
+        partialsDir: path.join(__dirname, '../views/partials'),
+        defaultLayout: 'main',
+    })
+)
+
+app.enable('view cache')
+
+// Setup static directory to serve
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Home',
-        name: 'Mohammed Elzanaty',
-    })
+    // Serves the body of the page aka "index.handlebars" to the container //aka "main.handlebars"
+    res.render('index', { template: 'Home' })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'Help',
-        name: 'Mohammed Elzanaty',
-    })
+    res.render('about', { template: 'About' })
 })
 
 app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help',
-        name: 'Mohammed Elzanaty',
-    })
+    res.render('help', { template: 'About', helpText: 'This is your help text' })
 })
 
 app.get('/weather', (req, res) => {
     res.json({
         name: 'Mohammed',
         age: 26,
+    })
+})
+
+// Handle 404
+app.use((req, res) => {
+    res.status(404).render('404', { template: '404: File Not Found', url: req.url.slice(1) })
+})
+
+// Handle 500
+app.use((error, req, res) => {
+    res.status(500).render('500', {
+        template: '500: Internal Server Error',
+        errorMsg: error.message,
+        url: req.url.slice(1),
     })
 })
 
