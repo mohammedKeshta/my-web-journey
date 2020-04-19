0d969fc69c86b4d93b13733011be1b4d135b1afa
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-04-19 13:57:50 +0200

    UDEMY: react-2nd-edition => Section 8 Styling React - Architecture and Header Styles

--- Udemy/react-2nd-edition/indecision-app/src/Components/Header.js
+++ Udemy/react-2nd-edition/indecision-app/src/Components/Header.js
@@ -2,9 +2,9 @@ import React  from 'react'
 
 const Header = ({ title, subtitle }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      {subtitle && <h2>{subtitle}</h2>}
+    <div className='header'>
+      <h1 className='header__title'>{title}</h1>
+      {subtitle && <h2 className="header__subtitle">{subtitle}</h2>}
     </div>
   );
 };
