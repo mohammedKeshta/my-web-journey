2349f1208dd0d60f071508f585e689044b8487ea
mohammedelzanaty <mohammedelzanat129@gmail.com>
2020-05-16 23:43:11 +0200

    LinkedIn: Become a Programmer Foundation - Programming Foundation Algorithms -> Section: Overview

--- Linkedin/linkedin-become-a-programmer-foundations/4.programming-foundations-algorithms/1 Introduction/gcd_start.py
+++ Linkedin/linkedin-become-a-programmer-foundations/4.programming-foundations-algorithms/1 Introduction/gcd_start.py
@@ -3,9 +3,15 @@
 
 
 def gcd(a, b):
-    pass
-    
-        
+    small = min(a, b)
+    if b == 0:
+        return a
+    for i in range(1, small+1):
+        if a % i == 0 and b % i == 0:
+            gcd = i
+    return gcd
+
+
 # try out the function with a few examples
 print(gcd(60, 96))  # should be 12
 print(gcd(20, 8))   # should be 4
