d947aa3d0aef64b17b6ed88ed6700c22ef0250a5
mohammedelzanaty <mohammedelzanat129@gmail.com>
2020-05-19 20:34:11 +0200

    LinkedIn: Become a programmer Foundation - Google Clould Platform Essential Training 3 -> init with remove large files

--- Linkedin/random/google-cloud-platform-essential-training-3/4_big data_and_genomics/4a_BigQuery/aws-redshift-scripts/3-query-data.sql
+++
@@ -1,5 +0,0 @@
-select * from part
-select * from supplier
-select * from customer
-select * from dwdate
-select * from lineorder
--- Linkedin/random/google-cloud-platform-essential-training-3/6_AI-ML/ml_scripts/4_tensorflow/1a_keras-tf.sh
+++
@@ -1,8 +0,0 @@
-# placeholder for tutorials (can be run in Colabs)
-
-# https://www.tensorflow.org/tutorials/
-- Basic classification
- - Text classification
- - Regression
- - Overfitting and underfitting
- - Save and load
