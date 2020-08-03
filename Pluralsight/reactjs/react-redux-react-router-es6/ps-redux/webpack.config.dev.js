bf5a9d755a0c74c1da5de3ab447370af9c516782
Mohammd Elzanaty <mohammdelzanaty@Mohammds-MacBook-Pro.local>
2020-08-03 14:13:39 +0200

    Pluralsight: React Path -> React Redux : Setup webpack env

---
+++ Pluralsight/reactjs/react-redux-react-router-es6/ps-redux/webpack.config.dev.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.join(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    https: false,
+    headers: { "Access-control-allow-origin": "*" },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
