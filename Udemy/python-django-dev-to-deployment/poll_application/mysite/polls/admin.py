5ed8da01a6b7ea8512ff81cade4a76d31c3ccf49
Mohammd Elzanaty <mohammdelzanaty@Mohammds-MacBook-Pro.local>
2020-10-26 22:04:09 +0200

    Django Documnetation: static files

--- Udemy/python-django-dev-to-deployment/poll_application/mysite/polls/admin.py
+++ Udemy/python-django-dev-to-deployment/poll_application/mysite/polls/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    
+
+
+admin.site.register(Question, QuestionAdmin)
