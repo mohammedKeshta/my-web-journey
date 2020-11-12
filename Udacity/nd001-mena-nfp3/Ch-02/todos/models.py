20a632df1213bd433cde5e2ebb7c88663940961d
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2020-11-12 12:50:59 +0200

    UDACITY: nd001-mena-nfp3 - add new relation todolists

--- Udacity/nd001-mena-nfp3/Ch-02/todos/models.py
+++ Udacity/nd001-mena-nfp3/Ch-02/todos/models.py
@@ -1,12 +1,24 @@
 from app import db
 
 
+# Create TodoList
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    todos = db.relationship('todo', backref='list', lazy=True)
+
+    def __repr__(self):
+        return 'Todo list {}: {}'.format(self.id, self.name)
+
+
 # Create TodoModel
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean(), nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return 'Todo {}: {}'.format(self.id, self.description)
