85f6a69d34a409895b5dfa9ef22b8f2db315152d
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-04-10 22:26:12 +0200

    Udacity: React Nano Degree - add nav and tweet details

---
+++ Udacity/react-nd001-mena-nfp3/redux/twitter/src/components/index.js
@@ -0,0 +1,5 @@
+import Nav from './Nav'
+import NewTweet from './NewTweet'
+import Tweet from './Tweet'
+
+export { Nav, NewTweet, Tweet }
---
+++ Udacity/react-nd001-mena-nfp3/redux/twitter/src/views/index.js
@@ -0,0 +1,4 @@
+import Dashboard from './Dashboard'
+import TweetDetails from './TweetDetails'
+
+export { Dashboard, TweetDetails }
